14f6efa70bf530ef6b07f8d864d4208e8a0a8c96
sreeja-create <alesreeja1@gmail.com>
2026-06-13 09:54:13 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -1,1 +1,12 @@
-
+import java.util.Scanner
+  public  class Add{
+    public static void main{
+      Scanner sc=new Scanner;
+      System.out.println("enter a no: ");
+        int a=sc.nextInt();
+        System.out.println("enter a no: ");
+        int b=sc.nextInt();
+        int add=a+b;
+        System.out.println("addition="+add);
+    }
+  }
