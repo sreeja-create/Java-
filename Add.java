32c199f4f7591c51bac23671864142735bae9519
sreeja-create <alesreeja1@gmail.com>
2026-06-13 10:17:46 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
   public  class Add{
-    public static void main{
-      Scanner sc=new Scanner;
+    public static void main(String[] arg){
+      Scanner sc=new Scanner(System.in);
       System.out.println("enter a no: ");
         int a=sc.nextInt();
         System.out.println("enter a no: ");
@@ -14,8 +14,8 @@ import java.util.Scanner;
   calcualte avg and sum of subj marks
   import java.util.Scanner;
   public  class Sum{
-    public static void main{
-      Scanner sc=new Scanner;
+    public static void main(String[] args){
+      Scanner sc=new Scanner(System.in);
       System.out.println("enter marks: ");
         double s1=sc.nextDouble();
         System.out.println("enter marks: ");
@@ -27,19 +27,3 @@ import java.util.Scanner;
     }
   }
 -------------
-import java.util.Scanner
-  public  class Swap{
-    public static void main{
-      Scanner sc=new Scanner;
-      int a=5;
-     int b=6;
-        int temp=a;
-        a=b;
-       b=temp;
-
-        System.out.println("a,b="+a,b);
-    }
-  }
----------
-
-
