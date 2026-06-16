d1f0888d0fff7c7e079a50b94ff3b6a2c901e1b8
sreeja-create <alesreeja1@gmail.com>
2026-06-16 14:59:05 +0530

    Update conditional statements.java

--- conditional statements.java
+++ conditional statements.java
@@ -95,3 +95,50 @@ import java.util.Scanner;
     }
     }
 -----------
+switch cases;-
+import java.util.Scanner; 
+public class Main {
+    public static void main(String[] args) {
+  Scanner sc=new Scanner(System.in);
+  int a=sc.nextInt();
+  switch(a)
+  {
+      case 1:
+          System.out.print("sunday");
+          break;
+  
+  case 2:
+      
+        System.out.println("monday.");
+        break;
+    
+    case 3:
+      
+        System.out.println("tuesday");
+        break;
+    
+    case 4:
+      
+        System.out.println("Wednesday");
+        break;
+    
+    case 5:
+      
+        System.out.println("Thursday.");
+        break;
+    
+    case 6:
+      
+        System.out.println("Friday");
+        break;
+      
+    case 7:
+      
+        System.out.println("Saturday");
+        break;
+    
+    default:
+    System.out.println("invalid");
+    }
+   }
+---------
