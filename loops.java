7d1da53fc8ba06efb89e9bd10034dc5383ce96a3
sreeja-create <alesreeja1@gmail.com>
2026-06-18 17:29:11 +0530

    Update loops.java

--- loops.java
+++ loops.java
@@ -12,4 +12,19 @@ public class Main {
     }
 }
 ---------
+ import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a no ");
+        int a=sc.nextInt();
+        int fact=1;
+        for(int i=1;i<=a;i++)
+        {
+            fact=fact*i;
+        }
+            System.out.println(fact);
+    }
+}
+-----------
 
