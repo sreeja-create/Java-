b774486e36be3fe43c749ffa9aaa04dffa3b060c
sreeja-create <alesreeja1@gmail.com>
2026-06-19 08:23:23 +0530

    Update loops.java

--- loops.java
+++ loops.java
@@ -82,3 +82,18 @@ public class Main {
     }
 }
 ------
+print a multiplication table
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("entera no: ");
+        int n=sc.nextInt();
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(n*i);
+            
+        }
+    }
+}
+-------
